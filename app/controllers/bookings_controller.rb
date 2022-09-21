b8a5e5d8fe53feccaa5df8e52625347a664e5cf8
Adam Taber <altaber@gmail.com>
2022-09-21 11:06:20 -0500

    submit booking form with passenger info

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,27 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @passenger_quantity = params[:ticket_quantity].to_i
+    @passenger_quantity.times { @booking.passengers.build }
   end
 
   def create
     @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:flight_id, :ticket_quantity)
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :email])
   end
 
 end
